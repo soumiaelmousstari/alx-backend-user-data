95155e1e3934031fba67deae76d3c3e59001301d
SOUMIA_EL_MOUSSTARI <elmousstarisoumia@gmail.com>
2024-08-29 15:36:56 +0000

    0. Regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""A module for filtering logs.
+"""
+import os
+import re
+import logging
+import mysql.connector
+from typing import List
+
+
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str,
+        ) -> str:
+    """Filters a log line.
+    """
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
