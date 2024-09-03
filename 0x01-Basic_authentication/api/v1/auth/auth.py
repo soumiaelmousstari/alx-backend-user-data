6e9cce9713a8c4eaa04ebb6a4a785d118559f898
SOUMIA_EL_MOUSSTARI <elmousstarisoumia@gmail.com>
2024-09-03 19:25:02 +0000

    3. Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,20 +7,17 @@ from flask import request
 
 
 class Auth:
-    """Authentication class.
-    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Checks if a path requires authentication.
         """
-        if path is not None and excluded_paths is not None:
-            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
-                pattern = ''
-                if exclusion_path[-1] == '*':
-                    pattern = '{}.*'.format(exclusion_path[0:-1])
-                elif exclusion_path[-1] == '/':
-                    pattern = '{}/*'.format(exclusion_path[0:-1])
-                else:
-                    pattern = '{}/*'.format(exclusion_path)
-                if re.match(pattern, path):
-                    return False
-        return True
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Gets the authorization header field from the request.
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Gets the current user from the request.
+        """
+        return None
