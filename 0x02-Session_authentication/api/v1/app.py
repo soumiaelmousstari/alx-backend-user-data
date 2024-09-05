9e63a054e1816e2d92d2024cac453d477dd44f97
SOUMIA_EL_MOUSSTARI <elmousstarisoumia@gmail.com>
2024-09-05 12:43:22 +0000

    5. Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -64,8 +64,6 @@ def authenticate_user():
             if auth_header(request) is None and \
                     auth.session_cookie(request) is None:
                 abort(401)
-            if auth_header is None:
-                abort(401)
             if request.current_user is None:
                 abort(403)
 
