a008f14a70567638b030049f8d49ea9d1684d3ed
SOUMIA_EL_MOUSSTARI <elmousstarisoumia@gmail.com>
2024-09-05 12:02:24 +0000

    0. Et moi et moi et moi!

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -56,7 +56,7 @@ def authenticate_user():
         ]
         if auth.require_auth(request.path, excluded_paths):
             auth_header = auth.authorization_header(request)
-            user = auth.current_user(request)
+            request.current_user = auth.current_user(request)
             if auth_header is None:
                 abort(401)
             if user is None:
